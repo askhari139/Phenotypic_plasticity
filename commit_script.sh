a9be7506f4adc2078ecec5c2f0027e7a6df31010
askhari139 <askhari139@gmail.com>
2020-10-13 15:18:42 +0530

    generic

--- commit_script.sh
+++ commit_script.sh
@@ -1,3 +1,3 @@
 git add -A
 git commit -m "$1"
-git push https://askhari139:ananth_139@github.com/askhari139/Projects.git master
+git push https://askhari139:ananth_139@github.com/askhari139/Phenotypic_plasticity.git master
